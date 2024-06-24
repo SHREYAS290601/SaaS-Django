45ce2cc607b6af2a787d6c52654c8969f43e7c7d
Shreyas Kulkarni <shreyas290601@gmail.com>
2024-06-24 16:17:53 +0530

    Init: Initial push for SaaS Django application

---
+++ base/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+from visits.models import PageVisit
+
+
+def home(request):
+    qs = PageVisit.objects.all()
+    page_qs = PageVisit.object.filter(path=request.path)
+    qs.count()
+    page_qs.count()
+
+    content = {
+        "title": "Home",
+        "content": "Welcome to the homepage.",
+    }
+    PageVisit.objects.create(path=request.path)
+    html_page = "home.html"
+    return render(request, html_page, content)
